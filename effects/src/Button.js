3f13197f62894f592347db87dac469a7351805d7
ericagong <48196721+ericagong@users.noreply.github.com>
2022-03-24 19:04:10 +0900

    useEffect()

--- effects/src/Button.js
+++ effects/src/Button.js
@@ -1,16 +1,12 @@
-import PropTypes from "prop-types";
-import styles from "./Button.module.css"; // css can be modulized by React and Component.module.css file.
+import PropTypes from 'prop-types'
+import styles from './Button.module.css' // css can be modulized by React and Component.module.css file.
 
-function Button({text}) {
-    return (
-        <button className={styles.btn}>
-            {text}
-        </button>
-    )
+function Button({ text }) {
+  return <button className={styles.btn}>{text}</button>
 }
 
 Button.propTypes = {
-    text: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 }
 
-export default Button;
+export default Button
